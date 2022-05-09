9034de36d3be968959f28eed478b96567eea0392
James Keicher <jamesdkeicher@gmail.com>
2022-05-09 10:01:37 -0700

    Setup: App wrapped in BrowserRouter in index. Testing skeleton built.

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { Switch, Route } from 'react-router-dom';
+
+export default function App() {
+  return <h1>Hello World</h1>;
+}
---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+
+describe('Should test router for Rick and Morty API', () => {
+  it('Should fetch a list of characters from API', async () => {
+    const res = null;
+  });
+});
